02fdd8e2915fd521d482f384cb50df8719d8dd97
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-05-26 10:54:10 +0200

    Add return types and fix minor issues

--- labs/Python/lab1.py
+++ labs/Python/lab1.py
@@ -1,9 +1,9 @@
 # Task 1
-def find_position(string: str, key: str):
+def find_position(string: str, key: str) -> list:
     return [position for position, char in enumerate(string) if char == key]
 
 
-def find_positions(string: str, *keys):
+def find_positions(string: str, *keys) -> dict:
     positions = {}
     for key in keys:
         position = find_position(string, key)
@@ -13,7 +13,7 @@ def find_positions(string: str, *keys):
 
 
 # Task 2
-def positions_to_tuple_list(positions: dict):
+def positions_to_tuple_list(positions: dict) -> list:
     tuple_list = []
     for char in positions:
         for position in positions[char]:
@@ -21,7 +21,7 @@ def positions_to_tuple_list(positions: dict):
     return sorted(tuple_list)
 
 
-def positions_to_tuple_list_comprehension(positions: dict):
+def positions_to_tuple_list_comprehension(positions: dict) -> list:
     return sorted([(pos, key) for key in positions for pos in positions[key]])
 
 
@@ -31,32 +31,32 @@ def dict_sorted_generator(input_dict: dict):
 
 
 # Task 4
-def draw_histo(input_dict: dict):
-    new_dict = {key: len(input_dict[key]) for key in input_dict}
+def draw_histo(input_dict: dict) -> None:
+    new_dict = {key: len(value) for key, value in input_dict.items()}
     for key, value in dict_sorted_generator(new_dict):
         print(key, "*" * value)
 
 
 # Task 5
-def convert_to_unique(input_list: list):
+def convert_to_unique(input_list: list) -> list:
     return [*set(input_list)]
 
 
 # Task 6
-def jaccard_index(set_1: set, set_2: set):
+def jaccard_index(set_1: set, set_2: set) -> float:
     return len(set_1 & set_2) / len(set_1 | set_2)
 
 
 # Task 7
-def evaluate_sets(set_1: set, set_2: set):
+def evaluate_sets(set_1: set, set_2: set) -> tuple:
     return (set_1, set_2) if len(set_1) >= len(set_2) else (set_2, set_1)
 
 
-def remove_from_bigger_set(bigger: set, smaller: set):
-    return bigger.remove((bigger - smaller).pop())
+def remove_from_bigger_set(bigger: set, smaller: set) -> None:
+    bigger.remove((bigger - smaller).pop())
 
 
-def optimize_jaccard_index(set_1: set, set_2: set, limit=0.9):
+def optimize_jaccard_index(set_1: set, set_2: set, limit=0.9) -> float:
     index = jaccard_index(set_1, set_2)
     while set_1 and set_2 and index < limit:
         try:
@@ -97,7 +97,15 @@ def main():
     print(jaccard_index(s1, s2))
 
     # Task 7
-    print(optimize_jaccard_index(s1, s2, 2))
+    set_1 = {0, 5, 4}
+    set_2 = {5, 2, 4}
+    optimize_jaccard_index(set_1, set_2, 0.55)
+    print(set_1, set_2, jaccard_index(set_1, set_2))
+
+    set_1 = {0, 5, 4}
+    set_2 = {5, 2, 4}
+    optimize_jaccard_index(set_1, set_2)
+    print(set_1, set_2, jaccard_index(set_1, set_2))
 
 
 if __name__ == "__main__":
